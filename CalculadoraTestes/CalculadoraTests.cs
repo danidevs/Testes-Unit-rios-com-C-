50be9e5e29f4891b952c814c7acd6a06d6a675ec
unknown <danielsk4te@hotmail.com>
2023-07-18 16:15:38 -0300

    testes unitário

---
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -0,0 +1,34 @@
+
+using Calculadora.Services;
+
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTests
+{
+    private CalculadoraImp _calc;
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImp();
+    }
+
+    [Fact]
+    public void  DeveSomar5Com10ERetornar15()
+    {
+        //Arrange
+        int num1 = 5;
+        int num2 = 10;
+        
+        // Act
+        int resultado = _calc.Somar(num1, num2);
+        
+        //Assert
+        Assert.Equal (15, resultado);
+    }
+
+}
+
+
+
+
+  
